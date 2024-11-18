b19ced4239d6392f017cd6b466c95fee13b6ab6a
ademcan <ademcaniyi7@gmail.com>
2024-11-18 19:42:03 +0300

    başlangıç

--- README.md
+++
@@ -1,2 +0,0 @@
-# to-do-list
-Yapılacaklarınızı düzenleyebileceğiniz not alabileceğiniz bir uygulama.
--- todo-app-backend/src/routes/todoRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getTodos, addTodo } from '../controllers/todoController';
-
-const router = Router();
-
-router.get('/', getTodos);
-router.post('/', addTodo);
-
-export default router;
